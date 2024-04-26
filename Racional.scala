0a172537f644125baa2b99e166ba4ad71207da77
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2024-04-26 15:28:10 -0300

    adicionei método para somar dois números racionais

--- Racional.scala
+++ Racional.scala
@@ -1,6 +1,14 @@
-
-
 class Racional(n: Int, d: Int):
 
   // validar denominador (não pode ser igual a zero)
+  require(d != 0)
+
+  val num: Int = n
+  val den: Int = d
+
+  // função para adicionar dois números racionais
+  def add(that: Racional): Racional = {
+    Racional(num * that.den + den * that.num, den * that.den)
+  }
+
   override def toString = s"$n/$d"
